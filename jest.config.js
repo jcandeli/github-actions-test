fc4731d2edeb0ec0e62eb5543333f64358fba814
JP <jp@MacBook-Pro-2.local>
2021-11-02 10:14:46 -0700

    low test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,4 @@
-// Custom config for Jest
-// We need special configuration to handle css modules and SVGS.
-// We have a special mock of the app.json file used for tests so that we can avoid the environment lookup
-
 module.exports = {
-  testEnvironment: 'jest-environment-jsdom',
-  moduleDirectories: ['node_modules', 'src'],
-  modulePaths: ['<rootDir>/src'],
-  moduleNameMapper: {
-    '^lodash-es$': 'lodash',
-    '\\.(css|less|scss|sss|styl)$': '<rootDir>/node_modules/jest-css-modules',
-    appConfig: '<rootDir>/config/app.test.json',
-    '^.+\\.svg$': 'jest-svg-transformer',
-    '\\.(jpg|ico|jpeg|png)$': '<rootDir>/src/helpers/__mocks__/files'
-  }
+  testEnvironment: "jest-environment-jsdom",
+  collectCoverageFrom: ["src/components/**/*.{js,jsx}"],
 };
