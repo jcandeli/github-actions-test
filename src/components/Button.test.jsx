de7ccd3fdae310f6f747de1f519dd8373e7b5baa
JP <jp@MacBook-Pro-2.local>
2021-11-02 09:49:53 -0700

    moved files

--- src/components/Button.test.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import { Button } from "./Button";
-
-it("should render a snapshot", () => {
-  const { container } = render(<Button>Test Button</Button>);
-  expect(container).toMatchSnapshot();
-});
