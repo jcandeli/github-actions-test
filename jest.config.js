f686be55c63ed5618f8fd13429bcead76f095ce4
JP <jp@MacBook-Pro-2.local>
2021-11-02 14:53:19 -0700

    added coverageThreshold

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,13 @@
 module.exports = {
   testEnvironment: "jest-environment-jsdom",
   collectCoverageFrom: ["src/components/**/*.{js,jsx}"],
+  coverageReporters: ["clover", "json", "text-summary"],
+  coverageThreshold: {
+    "./src/components/": {
+      branches: 75,
+      functions: 75,
+      lines: 75,
+      statements: 75,
+    },
+  },
 };
